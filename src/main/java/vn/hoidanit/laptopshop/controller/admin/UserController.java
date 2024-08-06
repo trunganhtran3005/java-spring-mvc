9f37679334335e489d84892c242f345b3ad7cb0d
trunganhtran3005 <anhtrung053004@gmail.com>
2024-08-07 05:55:21 +0700

    validate

--- src/main/java/vn/hoidanit/laptopshop/controller/admin/UserController.java
+++ src/main/java/vn/hoidanit/laptopshop/controller/admin/UserController.java
@@ -5,6 +5,8 @@ import java.util.List;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.FieldError;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -12,6 +14,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.multipart.MultipartFile;
 
+import jakarta.validation.Valid;
 import vn.hoidanit.laptopshop.domain.User;
 import vn.hoidanit.laptopshop.service.UploadService;
 import vn.hoidanit.laptopshop.service.UserService;
@@ -67,17 +70,23 @@ public class UserController {
 
     // button_create_user
     @PostMapping("/admin/user/create")
-    public String create(Model model, @ModelAttribute("newUser") User anhtrung,
+    public String create(Model model, @ModelAttribute("newUser") @Valid User anhtrung,
+            BindingResult bindingResult,
             @RequestParam("file") MultipartFile file) {
+
+        // validate
+        List<FieldError> errors = bindingResult.getFieldErrors();
+        for (FieldError error : errors) {
+            System.out.println(error.getObjectName() + " - " + error.getDefaultMessage());
+        }
+        //
         String avatar = this.UploadService.handleUploadFile(file, "avatar");
-
         String hashpassword = this.passwordEncoder.encode(anhtrung.getPassword());
-
         anhtrung.setAvatar(avatar);
         anhtrung.setPassword(hashpassword);
-
         anhtrung.setRole(this.userService.getRolebyName(anhtrung.getRole().getName()));
         this.userService.handleSaveUser(anhtrung);
+
         return "redirect:/admin/user";
     }
 
--- src/main/java/vn/hoidanit/laptopshop/domain/User.java
+++ src/main/java/vn/hoidanit/laptopshop/domain/User.java
@@ -10,6 +10,9 @@ import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
 import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotNull;
 
 @Entity
 @Table(name = "users")
@@ -18,10 +21,18 @@ public class User {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private long id;
 
+    @NotNull
+    @Email
     private String email;
 
+    @NotNull
+    @Min(2)
     private String password;
+
+    @NotNull
+    @Min(2)
     private String fullName;
+
     private String address;
     private String phone;
 
